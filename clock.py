9a2b13fe3784177b927692d1e93581b0b1d660a6
Amit kumar gupta <kumarguptaamit716@gmail.com>
2021-10-31 16:19:07 +0530

    First commit

---
+++ clock.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import *
+import random
+
+
+root = Tk()
+root.configure(background = 'black')
+root.geometry("380x120+0+0")
+root.overrideredirect(1)
+
+def time():
+    timer = strftime('%I:%M:%S %p')
+    date = strftime('%d/%B/%Y')
+
+    string = f"{date} \n{timer}"
+
+    label.config(text = string)
+    label.after(1000,time)
+
+cols = ['white', 'green', 'purple', 'blue', 'yellow', 'red', 'orange', 'cyan' , 'pink','light green']
+rand_color = random.choice(cols)
+label = Label(root, font=("ds-digital", 40), background = "black", foreground = rand_color)
+label.pack(anchor='center')
+time()
+
+root.mainloop()
